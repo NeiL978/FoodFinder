a0d5725c87650e1f8b6fd4258f56948a2101852a
NeiL Liu <godblessbaby@gmail.com>
2017-05-13 00:34:31 +0800

    新增點選cell時的互動，及修正accessoryType在reusableCell中重複的bug

--- FoodFinder/RestaurantTableViewController.swift
+++ FoodFinder/RestaurantTableViewController.swift
@@ -16,8 +16,9 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
     
-    var restaurantType = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "American", "Breakfase & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    var restaurantType = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfase & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
     
+    var restaurantIsVisited = Array(repeating: false, count: 21)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,10 +53,64 @@ class RestaurantTableViewController: UITableViewController {
         cell.locationLabel.text = restaurantLocations[indexPath.row]
         cell.typeLabel.text = restaurantType[indexPath.row]
         cell.thumbnailImageView.image = restaurantImgs[indexPath.row]
-//        cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.bounds.height / 2
-//        cell.thumbnailImageView.clipsToBounds = true
-
+        cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.bounds.height / 2
+        cell.thumbnailImageView.clipsToBounds = true
+        
+        //判斷是否點選了check-in
+        cell.accessoryType = restaurantIsVisited[indexPath.row] ? .checkmark : .none //三元運算
+//        if restaurantIsVisited[indexPath.row] {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
+        
         return cell
+    }
+    
+    // cell被選取時
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let optionMenu = UIAlertController(title: nil, message: "What do you want to do?", preferredStyle: .actionSheet)
+        
+        // 實作CancelAction動作
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        // 實作CallAction動作
+        let callAction = UIAlertAction(title: "Call" + "123-000-\(indexPath.row)", style: .default) { (action) in
+            //實作點選Call時的Alert
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alertMessage, animated: true, completion: nil)
+        }
+        optionMenu.addAction(callAction)
+        
+        // 實作Check-in動作
+        let cell = tableView.cellForRow(at: indexPath)
+        
+        if restaurantIsVisited[indexPath.row] {
+            let undoCheckInAction = UIAlertAction(title: "Undo Check in", style: .default) { (action) in
+                
+                cell?.accessoryType = .none
+                self.restaurantIsVisited[indexPath.row] = false
+            }
+            optionMenu.addAction(undoCheckInAction)
+        } else {
+            let checkInAction = UIAlertAction(title: "Check in", style: .default) { (action) in
+                
+                cell?.accessoryType = .checkmark
+                
+                self.restaurantIsVisited[indexPath.row] = true
+                
+            }
+            optionMenu.addAction(checkInAction)
+        }
+        
+        present(optionMenu, animated: true, completion: nil)
+        
+        //取消cell被選取後殘留的灰色選取狀態
+        tableView.deselectRow(at: indexPath, animated: false)
+        
     }
     
 
