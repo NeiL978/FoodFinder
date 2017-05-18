52cebdcd55e114f6a1d10699e2845a4954e37ee7
NeiL Liu <godblessbaby@gmail.com>
2017-05-19 03:01:16 +0800

    新增Table Row的Swipe實現刪除、社群分享功能

--- FoodFinder/RestaurantTableViewController.swift
+++ FoodFinder/RestaurantTableViewController.swift
@@ -12,7 +12,9 @@ class RestaurantTableViewController: UITableViewController {
     
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
-    var restaurantImgs = [#imageLiteral(resourceName: "cafedeadend"), #imageLiteral(resourceName: "homei"), #imageLiteral(resourceName: "teakha"), #imageLiteral(resourceName: "cafeloisl"), #imageLiteral(resourceName: "petiteoyster"), #imageLiteral(resourceName: "forkeerestaurant"), #imageLiteral(resourceName: "posatelier"), #imageLiteral(resourceName: "bourkestreetbakery"), #imageLiteral(resourceName: "haighschocolate"), #imageLiteral(resourceName: "palominoespresso"), #imageLiteral(resourceName: "upstate"), #imageLiteral(resourceName: "traif"), #imageLiteral(resourceName: "grahamavenuemeats"), #imageLiteral(resourceName: "wafflewolf"), #imageLiteral(resourceName: "fiveleaves"), #imageLiteral(resourceName: "cafelore"), #imageLiteral(resourceName: "confessional"), #imageLiteral(resourceName: "barrafina"), #imageLiteral(resourceName: "donostia"), #imageLiteral(resourceName: "royaloak"), #imageLiteral(resourceName: "caskpubkitchen")]
+//    var restaurantImgs = [#imageLiteral(resourceName: "cafedeadend"), #imageLiteral(resourceName: "homei"), #imageLiteral(resourceName: "teakha"), #imageLiteral(resourceName: "cafeloisl"), #imageLiteral(resourceName: "petiteoyster"), #imageLiteral(resourceName: "forkeerestaurant"), #imageLiteral(resourceName: "posatelier"), #imageLiteral(resourceName: "bourkestreetbakery"), #imageLiteral(resourceName: "haighschocolate"), #imageLiteral(resourceName: "palominoespresso"), #imageLiteral(resourceName: "upstate"), #imageLiteral(resourceName: "traif"), #imageLiteral(resourceName: "grahamavenuemeats"), #imageLiteral(resourceName: "wafflewolf"), #imageLiteral(resourceName: "fiveleaves"), #imageLiteral(resourceName: "cafelore"), #imageLiteral(resourceName: "confessional"), #imageLiteral(resourceName: "barrafina"), #imageLiteral(resourceName: "donostia"), #imageLiteral(resourceName: "royaloak"), #imageLiteral(resourceName: "caskpubkitchen")]
+    
+    var restaurantImgs = ["cafedeadend.jpg", "homei.jpg", "teakha.jpg", "cafeloisl.jpg", "petiteoyster.jpg", "forkeerestaurant.jpg", "posatelier.jpg", "bourkestreetbakery.jpg", "haighschocolate.jpg", "palominoespresso.jpg", "upstate.jpg", "traif.jpg", "grahamavenuemeats.jpg", "wafflewolf.jpg", "fiveleaves.jpg", "cafelore.jpg", "confessional.jpg", "barrafina.jpg", "donostia.jpg", "royaloak.jpg", "caskpubkitchen.jpg"]
     
     var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
     
@@ -52,7 +54,7 @@ class RestaurantTableViewController: UITableViewController {
         cell.nameLabel.text = restaurantNames[indexPath.row]
         cell.locationLabel.text = restaurantLocations[indexPath.row]
         cell.typeLabel.text = restaurantType[indexPath.row]
-        cell.thumbnailImageView.image = restaurantImgs[indexPath.row]
+        cell.thumbnailImageView.image = UIImage(named: restaurantImgs[indexPath.row])
         cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.bounds.height / 2
         cell.thumbnailImageView.clipsToBounds = true
         
@@ -113,6 +115,52 @@ class RestaurantTableViewController: UITableViewController {
         
     }
     
+    // 此方法只能單純的新增或刪除row，用editActionsForRowAt才能設置更多的動作
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            restaurantNames.remove(at: indexPath.row)
+//            restaurantLocations.remove(at: indexPath.row)
+//            restaurantType.remove(at: indexPath.row)
+//            restaurantIsVisited.remove(at: indexPath.row)
+//            restaurantImgs.remove(at: indexPath.row)
+//        }
+//        print("總數：\(restaurantNames.count)")
+//        for name in restaurantNames {
+//            print(name)
+//        }
+//        tableView.deleteRows(at: [indexPath], with: .fade)
+//    }
+    
+    // 針對row進行swipe的後續動作設定（實現刪除、社群分享）
+    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        //社群分享按鈕
+        let shareAction = UITableViewRowAction(style: .default, title: "Share") { (action, indexPath) in
+            //設定預設分享文字
+            let defaultText = "Just checking in at \(self.restaurantNames[indexPath.row])"
+            //設定分享圖片 之後可修改為相機或相簿分享
+            if let imageToShare = UIImage(named: self.restaurantImgs[indexPath.row]) {
+                let activityController = UIActivityViewController(activityItems: [defaultText, imageToShare], applicationActivities: nil)
+                self.present(activityController, animated: true, completion: nil)
+            }
+        }
+        shareAction.backgroundColor = UIColor(red:0.114, green:0.631, blue:0.949, alpha:1.000)
+        
+        
+        //Delete Rows
+        let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { (action, indexPath) in
+            
+            self.restaurantNames.remove(at: indexPath.row)
+            self.restaurantLocations.remove(at: indexPath.row)
+            self.restaurantType.remove(at: indexPath.row)
+            self.restaurantIsVisited.remove(at: indexPath.row)
+            self.restaurantImgs.remove(at: indexPath.row)
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        deleteAction.backgroundColor = UIColor(red:0.933, green:0.502, blue:0.463, alpha:1.000)
+        
+        return [deleteAction, shareAction]
+    }
 
 }
 
